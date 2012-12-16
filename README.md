252cab913eef2fec935b9337b0e036b053bbd764
jgabios <jajalinux@gmail.com>
2012-12-16 10:30:22 +0200

    some readme info

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-a command line tool to tweet to your twitter account. it needs RingoJS.
+A command line tool to tweet to your twitter account. it needs RingoJS.
 
+Go to https://dev.twitter.com/apps, make an app, then go to the details page.
+you should see something like this:
+<img src = "https://googledrive.com/host/0B_djAcBpTBAccDF4N3Ayc2YxWDQ/twitter-dev-app.png" border="0"/>
+
+Take "Consumer key", "Consumer secret", "Access token" and "Access token secret", and put them
+in twit.js file.
+Make sure Access level is "Read and Write" in the settings page.
+that's it, you can now call the script and tweet:
+```bash
+sh tweet.sh "my tweet"
+```
--- twit.js
+++ twit.js
@@ -6,10 +6,10 @@ quit();
 var status = system.args[1].substring(0,140);
 
 var op = {
-       consumerKey : 'QGtGlK9DI32dRfDMywOvzg',
-       consumerSecret : 'MFotaZ7qc6twPYa3PsbkzKMBFusIB55nVuqsJHkqU',
-    accessTokenKey : '978038688-7QKaPqI7GU0j3unVUDyG23NupQ6CV2X9b0RQMmQc',
-    accessTokenSecret : 'ZGS5q9KtQbdzcuEcXESQS0ZkWjcoRqzKrf4jHoJL4'
+       consumerKey : 'O04uLA2kNYhp5PeYASvXXQ',
+       consumerSecret : 'GzesG4IT8ZItqPY3tmVyFL0j4skJTX1AmuT18alX3RU',
+    accessTokenKey : '35700271-EmFF2PV3D5w7Fz6Ld1dhot5ekkdBDXgZ72ONNKWAT',
+    accessTokenSecret : 'mHPxPsHGgYW2Si0pQptPPfpzzBrQyhg5J0tqovFeU'
       };
 
 var oauth = jsoauth.OAuth(op);
